271031613acab9f38bfbe22f1e8ff5c8197c54f7
zhlongfj <zhlongfj@sina.com>
2015-02-09 13:59:40 +0800

    refactor, introduce random to Player

--- java/maven/src/main/java/Player.java
+++ java/maven/src/main/java/Player.java
@@ -1,16 +1,20 @@
+import java.util.Random;
+
 /**
  * Created by zhl on 15/2/9.
  */
 public class Player {
     private int vitality;
     private String name;
+    private Random random;
 
-    public Player(String name, int i) {
+    public Player(String name, int i, Random random) {
         this.name = name;
+        this.random = random;
     }
 
     public void attack(Player play) {
-        play.reduceVitality(12);
+        play.reduceVitality(random.nextInt(10));
     }
 
     public void reduceVitality(int attackedVitiality) {
--- java/maven/src/test/java/PlayerTest.java
+++ java/maven/src/test/java/PlayerTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by zhl on 15/2/9.
@@ -8,8 +11,9 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class PlayerTest {
     @Test
     public void should_reduce_vitality_when_attacked() {
-        Player zhangsan = new Player("张三", 10);
-        Player lisi = new Player("李四", 20);
+        Random random = mock(Random.class);
+        Player zhangsan = new Player("张三", 10, random);
+        Player lisi = new Player("李四", 20, random);
         zhangsan.attack(lisi);
 
         assertThat(lisi.getVitality()).isLessThan(20);
