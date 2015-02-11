06c91c4f3aec4f5fc9917c56a4a0e32b50e00836
zhlongfj <zhlongfj@sina.com>
2015-02-11 23:28:22 +0800

    rename a parameter

--- java/maven/src/main/java/GameProcess.java
+++ java/maven/src/main/java/GameProcess.java
@@ -1,4 +1,5 @@
 import java.io.PrintStream;
+import java.util.Random;
 
 /**
  * Created by zhl on 15/2/9.
@@ -7,6 +8,13 @@ public class GameProcess {
     private PrintStream out;
     private Player firstAttackPlayer;
     private Player secondAttackPlayer;
+
+    public static void main(String[] args) {
+        Player player1 = new OrdinaryPlayer("张三", 10, new Random());
+        Player player2 = new Solider("李四", 20, new Weapon("优质木棒", 4), new Armor("铠甲", 4), new Random());
+        GameProcess game = new GameProcess(System.out, player1, player2);
+        game.start();
+    }
 
     public GameProcess(PrintStream out, Player firstAttackPlayer, Player secondAttackPlayer) {
         this.out = out;
@@ -42,10 +50,10 @@ public class GameProcess {
         printAttack(secondAttackPlayer, firstAttackPlayer);
     }
 
-    private void printAttack(Player attackPlayer, Player attackedPalyer) {
+    private void printAttack(Player attackPlayer, Player attackPlayer) {
         out.println(attackPlayer.getProfession() + attackPlayer.getName() + attackPlayer.getUsedWeapon()
-                + "攻击了" + attackedPalyer.getProfession() + attackedPalyer.getName() + ","
-                + attackedPalyer.getName() + "受到了" + attackedPalyer.getAttackedVitality() + "点伤害,"
-                + attackedPalyer.getName() + "剩余生命：" + attackedPalyer.getVitality());
+                + "攻击了" + attackPlayer.getProfession() + attackPlayer.getName() + ","
+                + attackPlayer.getName() + "受到了" + attackPlayer.getAttackedVitality() + "点伤害,"
+                + attackPlayer.getName() + "剩余生命：" + attackPlayer.getVitality());
     }
 }
